97230c26f788129ceb26ae802a46934e01ec107b
IvanKalyuzhko <kalyuzhkoivan99@gmail.com>
2024-04-26 13:41:48 +0300

    створив перший приклад сервера

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 const http = require('http')
 
-let server = http.createServer((req,res)=>{
-    
+let server = http.createServer((req,res)=>{ 
+    res.writeHead(200, {'Content-Type':'text/html; charset=utf-8'})
+    res.end('Hello <button>Кнопка</button>') 
+})
+
+const PORT = 3000 
+const HOST = 'localhost'
+
+server.listen(PORT, HOST, ()=>{ 
+    console.log(`Сервер працює http://${HOST}:${PORT}`) 
 })
