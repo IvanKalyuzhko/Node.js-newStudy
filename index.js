d4649f67db843e2965ed0a28191cf0d7278e3e23
IvanKalyuzhko <kalyuzhkoivan99@gmail.com>
2024-04-26 15:06:35 +0300

    створив 2 приклад сервера

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const http = require('http')
+const fs = require('fs')
 
 let server = http.createServer((req,res)=>{ 
     res.writeHead(200, {'Content-Type':'text/html; charset=utf-8'})
-    res.end('Hello <button>Кнопка</button>') 
+
+    if(req.url =='/') 
+        fs.createReadStream('./templates/index.html').pipe(res)
+    else if (req.url == '/about')
+        fs.createReadStream('./templates/about.html').pipe(res)
+    else
+        fs.createReadStream('./templates/error.html').pipe(res)
 })
+
 
 const PORT = 3000 
 const HOST = 'localhost'
