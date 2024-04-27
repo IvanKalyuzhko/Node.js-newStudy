e09cb46abc3db78020834d157a680522e9a9bd75
IvanKalyuzhko <kalyuzhkoivan99@gmail.com>
2024-04-27 12:51:31 +0300

    описав 3 сторінки початкового серверу

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
 const http = require('http')
-const fs = require('fs')
+const fs = require('fs') // підключаєм fs для роботи з файлами та папками
 
 let server = http.createServer((req,res)=>{ 
     res.writeHead(200, {'Content-Type':'text/html; charset=utf-8'})
 
+    //потрібно зчитувати файл в потоках даних тож користуємось createReadStream
+    //createReadStream - зчитує інформацію по кусочкам і отправляє по кусочкам інформацію на сервер (користувач одразу буде бачити інформацію на сторінці)
+    
+    //щоб відслідковувати одразу декілька url адресів можна зробити умову в якій будем звертатись до req
+    //та через цей параметр звертаємось до значення url і вказуєм наш адрес (url =='/')
     if(req.url =='/') 
-        fs.createReadStream('./templates/index.html').pipe(res)
+        // тут ми вказуєм потрібний шлях до файлу 
+        fs.createReadStream('./templates/index.html').pipe(res)//pipe бере ті кусочки інформації що вже зчитались і одразу відправляє на сервер(одразу показує користувачу)
     else if (req.url == '/about')
-        fs.createReadStream('./templates/about.html').pipe(res)
-    else
-        fs.createReadStream('./templates/error.html').pipe(res)
+        // тут ми вказуєм потрібний шлях до файлу 
+        fs.createReadStream('./templates/about.html').pipe(res)//pipe бере ті кусочки інформації що вже зчитались і одразу відправляє на сервер(одразу показує користувачу)
+    else //тут вказуєм любий інший url адрес
+        // тут ми вказуєм потрібний шлях до файлу 
+        fs.createReadStream('./templates/error.html').pipe(res)//pipe бере ті кусочки інформації що вже зчитались і одразу відправляє на сервер(одразу показує користувачу)
 })
 
 
